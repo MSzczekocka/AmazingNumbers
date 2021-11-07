6a1bece6bb5dabc05dc2dd4073c1cf434bd0a17d
Martyna <martyna.szczekocka@gmail.com>
2021-11-07 18:14:27 +0100

    handle wrong input

--- Amazing Numbers/task/src/numbers/request/NumberGetter.java
+++ Amazing Numbers/task/src/numbers/request/NumberGetter.java
@@ -1,18 +1,26 @@
 package numbers.request;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.function.DoubleToLongFunction;
 
 public class NumberGetter {
 
     public int getNumber() {
+        int number=0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a natural number:");
-        int number = scanner.nextInt();
+        do {
+            try {
+                System.out.println("Enter a natural number:");
+                number = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("This number is not natural!");
+            }
+            scanner.nextLine();
+        } while (number < 1);
 
-        if (number<1){
-            System.out.println("This number is not natural!");
-            System.exit(0);
-        }
         return number;
     }
+
+
 }
