a0c5dbfec403d75d618d84b03ad67c4b225b675e
Martyna <martyna.szczekocka@gmail.com>
2021-11-07 17:10:52 +0100

    Change display of the properties

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,14 +1,14 @@
 package numbers;
 
+import numbers.display.PropertiesDisplay;
 import numbers.request.NumberAnalyzer;
 import numbers.request.NumberGetter;
 
 public class Main {
     public static void main(String[] args) {
         NumberGetter numberGetter = new NumberGetter();
-        NumberAnalyzer numberAnalyzer = new NumberAnalyzer();
+        PropertiesDisplay propertiesDisplay = new PropertiesDisplay();
         int number = numberGetter.getNumber();
-        numberAnalyzer.checkBuzzNumber(number);
-        numberAnalyzer.checkParityNumber(number);
+        propertiesDisplay.displayProperties(number);
     }
 }
--- Amazing Numbers/task/src/numbers/request/NumberAnalyzer.java
+++ Amazing Numbers/task/src/numbers/request/NumberAnalyzer.java
@@ -2,24 +2,8 @@ package numbers.request;
 
 public class NumberAnalyzer {
 
-    public void checkBuzzNumber(int number) {
-        if (isDivisible(number)&&endWith7(number)){
-            System.out.println("It is a Buzz number");
-            System.out.println("Explanation:");
-            System.out.println(number + " is divisible by 7 and ends with 7");
-        }else if (isDivisible(number)){
-            System.out.println("It is a Buzz number");
-            System.out.println("Explanation:");
-            System.out.println(number + " is divisible by 7");
-        }else if (endWith7(number)){
-            System.out.println("It is a Buzz number");
-            System.out.println("Explanation:");
-            System.out.println(number + " ends with 7");
-        }else{
-            System.out.println("It is not a Buzz number.");
-            System.out.println("Explanation:");
-            System.out.println(number + " is neither divisible by 7 nor does it end with 7");
-        }
+    public boolean checkBuzzNumber(int number) {
+        return isDivisible(number) || endWith7(number);
     }
 
     private static boolean isDivisible(int number){
@@ -30,11 +14,7 @@ public class NumberAnalyzer {
         return numS.charAt(numS.length()-1)=='7';
     }
 
-    public void checkParityNumber(int number) {
-        if(number%2==0){
-            System.out.println("This number is Even.");
-        }else{
-            System.out.println("This number is Odd.");
-        }
+    public boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
